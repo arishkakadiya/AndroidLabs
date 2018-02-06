81fd7856549df6046ddb842a352839866c47193e
arish <arishkakadiya129@gmail.com>
2018-02-06 18:08:37 -0500

    Updated

--- app/src/main/java/com/example/arish/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/arish/androidlabs/ListItemsActivity.java
@@ -1,13 +1,55 @@
 package com.example.arish.androidlabs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ListItemsActivity extends Activity {
-
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        Log.i(ACTIVITY_NAME,"In OnCreate()");
+        ImageButton button = (ImageButton) findViewById(R.id.imageButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+                
+
+            }
+        });
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME,"In OnResume()");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME,"In OnStart()");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME,"In OnPause()");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME,"In OnStop()");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME,"In OnDestroy()");
     }
 }
